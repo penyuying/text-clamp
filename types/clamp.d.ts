720ddb9070faf0fee636ef4e3a2195a57a618ebd
penyuying <penyuying@qq.com>
2017-12-06 15:10:04 +0800

    修改types文件  Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up-to-date with 'origin/master'.
    
     Changes to be committed:
    	modified:   package.json
    	modified:   types/clamp.d.ts

--- types/clamp.d.ts
+++ types/clamp.d.ts
@@ -1,4 +1,4 @@
-export declare function clamp(element:HTMLElement, options:ClampOptions);
+export declare function clamp(element:HTMLElement, options?:ClampOptions);
 
 /**
  * 参数
@@ -7,10 +7,10 @@ export declare function clamp(element:HTMLElement, options:ClampOptions);
  * @interface ClampOptions
  */
 export interface ClampOptions{
-    clamp?:number=2;//最大行数
-    useNativeClamp?:boolean=true;//是否修剪
-    splitOnChars?:string|string[]=['.', '-', '–', '—', ' '] //Split on sentences (periods), hypens, en-dashes, em-dashes, and words (spaces).
-    animate?: boolean=false;//是否显示动画
-    truncationChar?: string='…';//修剪后显示的结束字符
+    clamp?:number;//最大行数
+    useNativeClamp?:boolean;//是否修剪
+    splitOnChars?:string|string[]; //Split on sentences (periods), hypens, en-dashes, em-dashes, and words (spaces).
+    animate?: boolean;//是否显示动画
+    truncationChar?: string;//修剪后显示的结束字符
     truncationHTML?:string;//修剪后显示的结束HTML
 }
